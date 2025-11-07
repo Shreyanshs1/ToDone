5566feabdc2b30ee177e3d5fa2c91d3cb11e9830
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-11-07 18:13:22 +0530

    fix: change production url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -7,7 +7,7 @@ const PRODUCTION_URL = 'https://todone-4vjo.onrender.com/api';
 const DEVELOPMENT_URL = '/api';
 
 // 3. Use Vite's "magic variable" to decide
-const baseURL = import.meta.env.PROD ? PRODUCTION_URL : DEVELOPMENT_URL;
+const baseURL = PRODUCTION_URL;
 
 const api = axios.create({
   baseURL: baseURL, // This is now dynamic!
